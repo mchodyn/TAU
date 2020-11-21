e3b46e01ab1ab2a044ec178fe5b513567b7bf5bd
Mateusz Chodyna <s17606@pjwstk.edu.pl>
2020-11-21 11:47:52 +0100

    add pepper seller test

--- cw2/pepper/best_sellers.py
+++ cw2/pepper/best_sellers.py
@@ -1,5 +1,32 @@
+import sys, os, time
 
+sys.path.insert(0, os.path.abspath('..'))
+
+from selenium.webdriver.common.keys import Keys
+
+from logger import log
 
 
 def best_sellers_test_pepper(driver):
-    print('a')
+    try:
+        log(20, "Starting pepper seller test for " + driver.name)
+
+        log(20, "Pepper seller test for " + driver.name + ' entering site')
+        driver.get("https://www.pepper.pl/")
+
+        log(20, "Pepper seller test for " + driver.name + ' opening menu and choosing seller')
+        driver.find_element_by_xpath('//*[@id="main"]/div[1]/header/div/div/div[2]/div').click()
+        driver.find_element_by_xpath('/html/body/section/div/nav/ul[3]/li[1]/a').click()
+
+        log(20, "Pepper seller test for " + driver.name + ' checking seller page')
+        assert 'Empik' in driver.title
+        assert 'Empik' in driver.find_element_by_xpath(
+            '//*[@id="main"]/div[1]/section[1]/header/div[2]/div[1]/div/div[2]/h1').text
+
+        driver.stop_client()
+        driver.quit()
+
+    except Exception as e:
+        log(40, 'Error occurred while running test for pepper seller for ' + driver.name + ' ' + str(e))
+    finally:
+        log(20, 'Finished test for pepper seller for ' + driver.name)
